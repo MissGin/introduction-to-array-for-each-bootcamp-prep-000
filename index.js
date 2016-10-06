86d558a4c280a62e9eeee376bae2b95cb225d355
Bonnie Brewer <bj.brewer@att.net>
2016-10-06 22:48:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function doToElemetnsInArray(array,callback) {
+  array.forEach(callback);
+}
+
+function changeCompletely(element,index,array) {
+  array[index]=(Math.random() * 100).toString() + '!!!'
+  }
+
+  var animals = ["dog","fish","cat"]
+
+  doToElementsInArray(animals,changeCompletely)
+
+  console.log(animals)
